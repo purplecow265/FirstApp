59d81c43dbb18ffdcf25899202b5e121fc56a410
Kemal Talen <kemaltalen@kemaltalensmbp.home>
2015-06-07 20:05:47 -0400

    revision

--- gitscript.py
+++ gitscript.py
@@ -1,6 +1,6 @@
 filename = raw_input("Enter location of file you want to add: ")
 message = raw_input("Enter a message: ")
-branchname = raw_input("Specify a branch")
+branchname = raw_input("Specify a branch (master is most common): ")
 from subprocess import call
 call(["git", "add", filename])
 call(["git", "commit", "-m", message])
